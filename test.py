c4bfda9a45a72dab0659078ee7c5d99eb3b12723
Devaev Maxim <mdevaev@gmail.com>
2013-11-19 15:28:12 +0400

    refactor

--- test.py
+++ test.py
@@ -2,26 +2,21 @@ from raava import task
 import time
 import logging
 
-def cont_sleep(cont, secs) :
+def cont_sleep(cont, secs):
     cont.switch("one sleep=%d" % (secs))
     time.sleep(secs)
     cont.switch("two sleep=%d" % (secs))
 
-def cont_notify(cont, *args_tuple) :
+def cont_notify(cont, *args_tuple):
     cont.switch(" ".join(map(str, args_tuple)))
 
-def foo(name, *args_tuple) :
-    for arg in args_tuple :
+def foo(name, *args_tuple):
+    for arg in args_tuple:
         notify(name, arg)
         sleep(2)
     notify(name, "finished!")
 
-class TaskManager(task.AbstractTaskManager) :
-    def on_save(self, task_id, state) : pass
-    def on_switch(self, task_id, retval) : pass
-    def on_error(self, task_id, err) : pass
-
-if __name__ == "__main__" :
+if __name__ == "__main__":
     logger = logging.getLogger(task.LOGGER_NAME)
     logger.setLevel(logging.DEBUG)
     handler = logging.StreamHandler()
@@ -29,10 +24,14 @@ if __name__ == "__main__" :
     handler.setFormatter(logging.Formatter("%(name)s %(threadName)s - %(levelname)s - %(message)s"))
     logger.addHandler(handler)
 
-    manager = TaskManager()
-    task.setup_builtins({ "sleep" : cont_sleep, "notify" : cont_notify })
+    manager = task.TaskManager(
+        lambda a, b: None,
+        lambda a, b: None,
+        lambda a, b: None,
+    )
+    task.setup_builtins({ "sleep": cont_sleep, "notify": cont_notify })
 
-    t1 = manager.add(foo, "task-1", 1, 2, 3, 4, 5)
+    t1 = manager.add(lambda: foo("task-1", 1, 2, 3, 4, 5))
     time.sleep(4)
-    t2 = manager.add(foo, "   task-2", 1, 2, 3)
+    t2 = manager.add(lambda: foo("   task-2", 1, 2, 3))
 
