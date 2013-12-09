ef40c82ca676c5dd248be3dc6764f63ac541a371
Devaev Maxim <mdevaev@gmail.com>
2013-12-09 18:51:36 +0400

    demo_api

---
+++ demo_api.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env pypy3
+
+import json
+import wsgiref.simple_server
+import cgi
+
+from raava import zoo
+from raava import rules
+from raava import events
+
+import gns
+
+def webapi(env_dict, start_response):
+    path = env_dict["PATH_INFO"]
+    client = zoo.connect(["localhost"])
+    events_api = events.EventsApi(client)
+    try:
+        if path == "/add":
+            data = env_dict["wsgi.input"].read(int(env_dict["CONTENT_LENGTH"]))
+            event_root = rules.EventRoot(json.loads(data.decode()))
+            retval = events_api.add_event(event_root, gns.HANDLER.ON_EVENT)
+        elif path == "/cancel":
+            events_api.cancel_event(cgi.parse_qs(env_dict["QUERY_STRING"])["job_id"][0])
+            retval = None
+        else:
+            raise Exception("Unknown method")
+    except Exception as err:
+        start_response("500 Internal Error", [("Content-Type", "text/plain")])
+        return [("ERROR: %s" % (str(err))).encode()]
+    finally:
+        client.stop()
+    start_response("200 OK", [("Content-Type", "text/plain")])
+    return [json.dumps({ "retval": retval }).encode()]
+
+httpd = wsgiref.simple_server.make_server("", 8080, webapi)
+httpd.serve_forever()
+
