811933b28b6e1d3e0637432456f87fd69c1e7717
Devaev Maxim <mdevaev@gmail.com>
2013-12-14 00:03:51 +0400

    demo_reinit -> reinit

---
+++ reinit.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env pypy3
+
+
+import sys
+
+from ulib import optconf
+
+from raava import const
+from raava import zoo
+from raava import service
+from raava import application
+
+
+##### Public methods #####
+def main():
+    config = optconf.OptionsConfig((
+            service.OPTION_LOG_FILE,
+            service.OPTION_LOG_LEVEL,
+            service.OPTION_ZOO_NODES,
+        ),
+        sys.argv[1:],
+        const.CONFIG_FILE,
+    )
+    config.add_arguments(
+        service.ARG_LOG_FILE,
+        service.ARG_LOG_LEVEL,
+        service.ARG_ZOO_NODES,
+    )
+    config.parser().add_argument("--do-it-now", dest="do_flag", action="store_true")
+    options = config.sync((service.MAIN_SECTION,))
+
+    if not options.do_flag:
+        raise RuntimeError("Specify option --do-it-now to process")
+
+    application.init_logging(
+        getattr(options, service.OPTION_LOG_LEVEL[1]),
+        getattr(options, service.OPTION_LOG_FILE[1]),
+    )
+    client = zoo.connect(getattr(options, service.OPTION_ZOO_NODES[1]))
+    for path in (zoo.INPUT_PATH, zoo.READY_PATH, zoo.RUNNING_PATH, zoo.CONTROL_PATH):
+        try:
+            client.delete(path, recursive=True)
+        except zoo.NoNodeError:
+            pass
+    zoo.init(client, True)
+
+
+##### Main #####
+if __name__ == "__main__":
+    main()
+
