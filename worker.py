06156a72da769f481628505d74f4f6f0cde8a6d5
Devaev Maxim <mdevaev@gmail.com>
2013-12-13 21:12:43 +0400

    demo_worker -> worker

---
+++ worker.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env pypy3
+
+
+from ulib import validators
+import ulib.validators.common # pylint: disable=W0611
+
+from raava import service
+from raava import rules
+
+from raava import apps
+import raava.apps.worker # pylint: disable=W0611
+
+import gns
+
+
+##### Public constants #####
+WORKER_SECTION = "worker"
+OPTION_QUEUE_TIMEOUT = ("queue-timeout", "queue_timeout", 1, lambda arg: validators.common.validNumber(arg, 0, value_type=float))
+ARG_QUEUE_TIMEOUT = ((OPTION_QUEUE_TIMEOUT[0],), OPTION_QUEUE_TIMEOUT, { "action" : "store", "metavar" : "<seconds>" })
+
+
+##### Public classes #####
+class WorkerMain(service.Main):
+    def construct(self, options):
+        rules.setup_builtins(gns.WORKER_BUILTINS_MAP)
+        return (
+            getattr(options, service.OPTION_ZOO_NODES[1]), # TODO: getattr
+            getattr(options, OPTION_QUEUE_TIMEOUT[1]),
+        )
+
+
+##### Main #####
+if __name__ == "__main__":
+    WorkerMain(
+        apps.worker.Worker,
+        WORKER_SECTION,
+        (OPTION_QUEUE_TIMEOUT,),
+        (ARG_QUEUE_TIMEOUT,),
+    ).run()
+
