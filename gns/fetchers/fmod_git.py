afb067a6971fa1e7c01ae5d8c19be010144aaebc
Devaev Maxim <mdevaev@gmail.com>
2014-02-13 03:35:21 +0400

    fmod_git: Using subprocess.check_output() and $PATH

--- gns/fetchers/fmod_git.py
+++ gns/fetchers/fmod_git.py
@@ -20,8 +20,6 @@ S_GIT = "git"
 O_REPO_URL  = "repo-url"
 O_REPO_DIR  = "repo-dir"
 O_REVISIONS = "revisions"
-O_GIT_BIN   = "git-bin"
-O_TAR_BIN   = "tar-bin"
 O_PREFIX    = "prefix"
 
 
@@ -30,28 +28,14 @@ _logger = logging.getLogger(LOGGER_NAME)
 
 
 ##### Private methods #####
-def _shell_exec(args):
-    _logger.debug("Running the command: %s", args)
-    proc = subprocess.Popen(
-        args,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
+def _shell_exec(command, **kwargs_dict):
+    proc_stdout = subprocess.check_output(
+        command.format(**kwargs_dict),
         env={ "LC_ALL": "C" },
+        universal_newlines=True,
         shell=True,
     )
-    (proc_stdout, proc_stderr) = proc.communicate()
-    proc_retcode = proc.returncode
-    message = "Command \"{cmd}\" results:\nStdout: {stdout}\nStderr: {stderr}\nReturn code: {retcode}".format(
-        cmd=args,
-        stdout=proc_stdout.strip(),
-        stderr=proc_stderr.strip(),
-        retcode=proc_retcode,
-    )
-    if proc_retcode != 0:
-        _logger.error(message)
-        raise RuntimeError("Command error")
-    else:
-        _logger.debug(message)
+    _logger.debug("Command stdout:\n{}".format(proc_stdout))
     return proc_stdout
 
 def _git_cleanup(rules_path, prefix, modules_list):
@@ -63,20 +47,18 @@ def _git_cleanup(rules_path, prefix, modules_list):
             shutil.rmtree(os.path.join(rules_path, module_name))
 
 def _git_update_rules(config_dict):
-    _shell_exec("{git} --work-tree {repo} --git-dir {repo}/.git pull".format(
-            git=config_dict[S_GIT][O_GIT_BIN],
-            repo=config_dict[S_GIT][O_REPO_DIR],
-        ))
+    _shell_exec("git --work-tree {repo} --git-dir {repo}/.git pull",
+        repo=config_dict[S_GIT][O_REPO_DIR],
+    )
 
     rules_path = config_dict[service.S_CORE][service.O_RULES_DIR]
     prefix = config_dict[S_GIT][O_PREFIX]
 
     modules_list = []
-    commits_list = _shell_exec("{git} --work-tree {repo} --git-dir {repo}/.git log -n {limit} --pretty=format:%H".format(
-            git=config_dict[S_GIT][O_GIT_BIN],
-            repo=config_dict[S_GIT][O_REPO_DIR],
-            limit=config_dict[S_GIT][O_REVISIONS],
-        )).decode().strip().split("\n")
+    commits_list = _shell_exec("git --work-tree {repo} --git-dir {repo}/.git log -n {limit} --pretty=format:%H",
+        repo=config_dict[S_GIT][O_REPO_DIR],
+        limit=config_dict[S_GIT][O_REVISIONS],
+    ).strip().split("\n")
     assert len(commits_list) > 0
     for commit in commits_list:
         module_name = prefix + commit
@@ -92,13 +74,11 @@ def _git_update_rules(config_dict):
         os.mkdir(tmp_path)
 
         _logger.info("Checkout %s --> %s", commit, module_path)
-        _shell_exec("{git} --work-tree {repo} --git-dir {repo}/.git archive {commit} | {tar} -x -C {tmp}".format(
-                git=config_dict[S_GIT][O_GIT_BIN],
-                repo=config_dict[S_GIT][O_REPO_DIR],
-                commit=commit,
-                tar=config_dict[S_GIT][O_TAR_BIN],
-                tmp=tmp_path,
-            ))
+        _shell_exec("git --work-tree {repo} --git-dir {repo}/.git archive {commit} | tar -x -C {tmp}",
+            repo=config_dict[S_GIT][O_REPO_DIR],
+            commit=commit,
+            tmp=tmp_path,
+        )
         os.rename(tmp_path, module_path)
 
     _git_cleanup(rules_path, prefix, modules_list)
@@ -112,8 +92,6 @@ CONFIG_MAP = {
         O_REPO_URL:  ("http://example.com", str),
         O_REPO_DIR:  ("/tmp",               lambda arg: validators.fs.valid_accessible_path(arg + "/.")),
         O_REVISIONS: (10,                   lambda arg: validators.common.valid_number(arg, 1)),
-        O_GIT_BIN:   ("/usr/bin/git",       validators.fs.valid_accessible_path),
-        O_TAR_BIN:   ("/usr/bin/tar",       validators.fs.valid_accessible_path),
         O_PREFIX:    ("git_",               str),
     },
 }
