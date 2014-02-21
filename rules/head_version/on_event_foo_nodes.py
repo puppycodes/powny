e91dd78d1846ca85c5aede943c6be92d8e8519ac
Devaev Maxim <mdevaev@gmail.com>
2014-02-21 18:55:55 +0400

    Refactoring: removed java-style copypaste from comparators, removed some unused logic from AbstractComparator(), in_list() and not_in_list() now accepts a list

--- rules/head_version/on_event_foo_nodes.py
+++ rules/head_version/on_event_foo_nodes.py
@@ -1,4 +1,4 @@
-@match_event(host=regexp(r"foo\d+"), status=in_list(STATUS.CRIT, STATUS.WARN))
+@match_event(host=cmp.regexp(r"foo\d+"), status=cmp.in_list(["CRIT", "WARN"]))
 def on_event(event) :
     email.send(("mdevaev@yandex-team.ru", "nbryskin@yandex-team.ru"), event)
     sms.send(("mdevaev", "nbryskin"), event)
