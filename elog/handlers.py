e496fa4089c88a8e0e04468d517602af51f9944f
Devaev Maxim <mdevaev@gmail.com>
2014-03-04 18:59:37 +0400

    elog: merged DictFormatter and ElasticHandler

--- elog/handlers.py
+++ elog/handlers.py
@@ -2,9 +2,7 @@ import socket
 import urllib.request
 import json
 import logging
-import time
-
-from . import formatters
+import datetime
 
 
 ##### Public classes #####
@@ -15,28 +13,61 @@ class ElasticHandler(logging.Handler):
             elastic:
                 level: DEBUG
                 class: elog.handlers.ElasticHandler
-                formatter: dict
-                url: http://example.com:9200/log-%Y-%m-%d/mdevaev-gns-elog2
+                url: http://example.com:9200/log-{utc:%Y}-{utc:%m}-{utc:%d}/gns2
+                time_field: "@timestamp"
+                time_format: "{utc:%Y}-{utc:%m}-{utc:%d}T{utc:%H}:{utc:%M}:{utc:%S}.{utc:%f}"
             ...
 
         URL components:
             example.com:9200 -- host/port
-            log-%Y-%m-%d -- index
-            gns-elog -- doctype
+            log-{utc:%Y}-{utc:%m}-{utc:%d} -- index
+            gns2 -- doctype
 
-        Requires formatter that returns dict instead of string.
+        The class does not use any formatters.
     """
 
-    def __init__(self, url, timeout=socket._GLOBAL_DEFAULT_TIMEOUT): # pylint: disable=W0212
+    def __init__(self, url, timeout=socket._GLOBAL_DEFAULT_TIMEOUT, time_field=None, time_format=None): # pylint: disable=W0212
         logging.Handler.__init__(self)
         self._url = url
         self._timeout = timeout
+        self._time_field = time_field
+        self._time_format = time_format
+
 
     def emit(self, record):
-        msg = self.format(record)
-        assert isinstance(msg, dict)
+        # Formatters are not used
+        msg = self._make_message(record)
         url = self._url.format(**msg)
-        url = time.strftime(url, time.gmtime(record.created))
+        del msg["utc"] # Remove the inner object utc
         request = urllib.request.Request(url, data=json.dumps(msg).encode())
         urllib.request.build_opener().open(request, timeout=self._timeout)
+
+    def _make_message(self, record):
+        msg = {
+            name: getattr(record, item)
+            for (name, item) in (
+                ("time",      "created"),
+                ("msecs",     "msecs"),
+                ("logger",    "name"),
+                ("level",     "levelname"),
+                ("level_no",  "levelno"),
+                ("msg",       "msg"),
+                ("args",      "args"),
+                ("file",      "pathname"),
+                ("line",      "lineno"),
+                ("func",      "funcName"),
+                ("pid",       "process"),
+                ("process",   "processName"),
+                ("tid",       "tid"), # Linux TID (from elog.records.LogRecord)
+                ("thread",    "threadName"),
+                ("thread_id", "thread"), # Python-specific thread id
+                ("exc_text",  "exc_text"), # Text exception
+                ("extra",     "extra"), # Custom fields
+            )
+            if hasattr(record, item)
+        }
+        msg["utc"] = datetime.datetime.utcfromtimestamp(record.created) # Only for config placeholders
+        if self._time_field is not None:
+            msg[self._time_field] = self._time_format.format(**msg)
+        return msg
 
