c995365a7023b3b7ad4c1260759fda074b253a97
Devaev Maxim <mdevaev@gmail.com>
2014-03-05 16:48:02 +0400

    elog.records: Fixed CFFI UserWarning: reimporting _cffi__x9ce693d7x39c3c66b might overwrite older definitions

--- elog/records.py
+++ elog/records.py
@@ -7,6 +7,10 @@ except ImportError:
     cffi = None
 
 
+##### Private objects #####
+_linux_syscall_lib = None
+
+
 ##### Public classes #####
 class LogRecord(logging.LogRecord):
     def __init__(self, *args, **kwargs):
@@ -17,15 +21,21 @@ class LogRecord(logging.LogRecord):
 ##### Private methods #####
 def _gettid():
     if sys.platform.startswith("linux") and cffi is not None:
+        return _linux_gettid()
+    else:
+        return None
+
+def _linux_gettid():
+    global _linux_syscall_lib
+    if _linux_syscall_lib is None:
         ffi = cffi.FFI()
         ffi.cdef("int syscall(int number);")
         lib = ffi.verify("""
                 #include <unistd.h>
                 #include <sys/syscall.h>
             """)
-        # 186 - Linux-specific syscall gettid()
-        tid = lib.syscall(186) # pylint: disable=E1101
-        return tid
-    else:
-        return None
+        _linux_syscall_lib = lib
+    # 186 - Linux-specific syscall gettid()
+    tid = _linux_syscall_lib.syscall(186) # pylint: disable=E1101
+    return tid
 
