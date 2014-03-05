b25b91f74f99a8c12b9a3d4973815941e260b3fc
Devaev Maxim <mdevaev@gmail.com>
2014-03-05 18:03:28 +0400

    elog.handlers: Removed task_done(), renamed retries_sleep to retry_interval

--- elog/handlers.py
+++ elog/handlers.py
@@ -34,14 +34,14 @@ class ElasticHandler(logging.Handler, threading.Thread):
             doctype
 
         Optional arguments:
-            time_field    -- Timestamp field name
-            time_format   -- Timestamp format
-            queue_size    -- The maximum size of the send queue, after which the caller thread is blocked
-            bulk_size     -- Number of messages in one session
-            retries       -- If the bulk will not be sent to N times, it will be lost
-            retries_sleep -- Delay between attempts to send
-            url_timeout   -- Socket timeout
-            log_timeout   -- Maximum waiting time of sending
+            time_field     -- Timestamp field name
+            time_format    -- Timestamp format
+            queue_size     -- The maximum size of the send queue, after which the caller thread is blocked
+            bulk_size      -- Number of messages in one session
+            retries        -- If the bulk will not be sent to N times, it will be lost
+            retry_interval -- Delay between attempts to send
+            url_timeout    -- Socket timeout
+            log_timeout    -- Maximum waiting time of sending
 
         The class does not use any formatters.
     """
@@ -55,7 +55,7 @@ class ElasticHandler(logging.Handler, threading.Thread):
             queue_size=512,
             bulk_size=512,
             retries=5,
-            retries_sleep=1,
+            retry_interval=1,
             url_timeout=socket._GLOBAL_DEFAULT_TIMEOUT, # pylint: disable=W0212
             log_timeout=5,
         ):
@@ -69,7 +69,7 @@ class ElasticHandler(logging.Handler, threading.Thread):
         self._time_format = time_format
         self._bulk_size = bulk_size
         self._retries = retries
-        self._retries_sleep = retries_sleep
+        self._retry_interval = retry_interval
         self._url_timeout = url_timeout
         self._log_timeout = log_timeout
 
@@ -109,7 +109,6 @@ class ElasticHandler(logging.Handler, threading.Thread):
             try:
                 while len(items) < self._bulk_size:
                     items.append(self._queue.get(timeout=max(wait_until - time.time(), 0)))
-                    self._queue.task_done()
             except queue.Empty:
                 wait_until = time.time() + self._log_timeout
 
@@ -171,7 +170,7 @@ class ElasticHandler(logging.Handler, threading.Thread):
                     _logger.exception("ElasticHandler could not send %d log records after %d retries", self._bulk_size, self._retries)
                     break
                 retries -= 1
-                time.sleep(self._retries_sleep)
+                time.sleep(self._retry_interval)
 
     def _json_dumps(self, obj):
         return json.dumps(obj, cls=_DatetimeEncoder, time_format=self._time_format)
