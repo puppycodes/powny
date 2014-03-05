d493759b5184cf9c3fff92dff4eed6211067a711
Devaev Maxim <mdevaev@gmail.com>
2014-03-05 17:41:33 +0400

    elog.records: Fixed race when constructing CFFI

--- elog/records.py
+++ elog/records.py
@@ -7,10 +7,6 @@ except ImportError:
     cffi = None
 
 
-##### Private objects #####
-_linux_syscall_lib = None
-
-
 ##### Public classes #####
 class LogRecord(logging.LogRecord):
     def __init__(self, *args, **kwargs):
@@ -19,23 +15,18 @@ class LogRecord(logging.LogRecord):
 
 
 ##### Private methods #####
-def _gettid():
+def _make_gettid():
     if sys.platform.startswith("linux") and cffi is not None:
-        return _linux_gettid()
-    else:
-        return None
-
-def _linux_gettid():
-    global _linux_syscall_lib
-    if _linux_syscall_lib is None:
         ffi = cffi.FFI()
         ffi.cdef("int syscall(int number);")
         lib = ffi.verify("""
                 #include <unistd.h>
                 #include <sys/syscall.h>
             """)
-        _linux_syscall_lib = lib
-    # 186 - Linux-specific syscall gettid()
-    tid = _linux_syscall_lib.syscall(186) # pylint: disable=E1101
-    return tid
+        # 186 - Linux-specific syscall gettid()
+        gettid = ( lambda: lib.syscall(186) ) # pylint: disable=E1101
+    else:
+        gettid = ( lambda: None ) # Fallback
+    return gettid
+_gettid = _make_gettid()
 
