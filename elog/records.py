277a76cd276b73aa11beb15af1d485e441c8349f
Devaev Maxim <mdevaev@gmail.com>
2014-03-06 16:01:10 +0400

    elog.record: Cached FQDN every minute

--- elog/records.py
+++ elog/records.py
@@ -16,7 +16,7 @@ class LogRecord(logging.LogRecord):
     def __init__(self, *args, **kwargs):
         logging.LogRecord.__init__(self, *args, **kwargs)
         self.tid = _gettid()
-        self.fqdn = _cached_getfqdn(int(time.time() / 3600)) # Cached value FQDN, updated every hour
+        self.fqdn = _cached_getfqdn(int(time.time() / 60)) # Cached value FQDN, updated every minute
         self.node = platform.uname()[1] # Nodename from uname
 
 
