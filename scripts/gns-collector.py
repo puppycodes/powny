8b9e0548bb8a7396ce649b7ddf27e1740a8ef2d0
Devaev Maxim <mdevaev@gmail.com>
2014-03-07 15:08:55 +0400

    scripts: Refactoring: variables naming

--- scripts/gns-collector.py
+++ scripts/gns-collector.py
@@ -8,21 +8,21 @@ from gns import service
 
 ##### Public methods #####
 def main():
-    config_dict = service.init(description="GNS Collector")[0]
-    core_dict = config_dict[service.S_CORE]
-    app_dict = config_dict[service.S_COLLECTOR]
+    config = service.init(description="GNS Collector")[0]
+    core_attrs = config[service.S_CORE]
+    app_attrs = config[service.S_COLLECTOR]
 
     app = application.Application(
         thread_class      = collector.CollectorThread,
-        workers           = app_dict[service.O_WORKERS],
-        die_after         = app_dict[service.O_DIE_AFTER],
-        quit_wait         = app_dict[service.O_QUIT_WAIT],
-        interval          = app_dict[service.O_RECHECK],
-        nodes_list        = core_dict[service.O_ZOO_NODES],
-        poll_interval     = app_dict[service.O_POLL_INTERVAL],
-        delay             = app_dict[service.O_ACQUIRE_DELAY],
-        recycled_priority = app_dict[service.O_RECYCLED_PRIORITY],
-        garbage_lifetime  = app_dict[service.O_GARBAGE_LIFETIME],
+        workers           = app_attrs[service.O_WORKERS],
+        die_after         = app_attrs[service.O_DIE_AFTER],
+        quit_wait         = app_attrs[service.O_QUIT_WAIT],
+        interval          = app_attrs[service.O_RECHECK],
+        nodes_list        = core_attrs[service.O_ZOO_NODES],
+        poll_interval     = app_attrs[service.O_POLL_INTERVAL],
+        delay             = app_attrs[service.O_ACQUIRE_DELAY],
+        recycled_priority = app_attrs[service.O_RECYCLED_PRIORITY],
+        garbage_lifetime  = app_attrs[service.O_GARBAGE_LIFETIME],
     )
     app.run()
 
--- scripts/gns-reinit.py
+++ scripts/gns-reinit.py
@@ -7,14 +7,14 @@ from gns import service
 
 ##### Public methods #####
 def main():
-    (config_dict, parser, argv_list) = service.init(description="Re-init ZooKeeper GNS storage")
+    (config, parser, argv_list) = service.init(description="Re-init ZooKeeper GNS storage")
     parser.add_argument("--do-it-now", dest="do_flag", action="store_true")
     options = parser.parse_args(argv_list)
 
     if not options.do_flag: # pylint: disable=E1101
         raise RuntimeError("Specify option --do-it-now to process")
 
-    with zoo.Connect(config_dict[service.S_CORE][service.O_ZOO_NODES]) as client:
+    with zoo.Connect(config[service.S_CORE][service.O_ZOO_NODES]) as client:
         zoo.drop(client, True)
         zoo.init(client, True)
 
