5f9c420cc26de2371a12b9b28ca99e00581b3a82
Devaev Maxim <mdevaev@gmail.com>
2014-03-07 16:25:10 +0400

    services: config_attrs -> config_opts

--- scripts/gns-collector.py
+++ scripts/gns-collector.py
@@ -11,19 +11,19 @@ from gns import service
 ##### Public methods #####
 def main():
     config = service.init(description="GNS Collector")[0]
-    app_attrs = config[service.S_COLLECTOR]
+    app_opts = config[service.S_COLLECTOR]
 
     app = application.Application(
         thread_class      = collector.CollectorThread,
         zoo_connect       = lambda: zclient.connect(config),
-        workers           = app_attrs[service.O_WORKERS],
-        die_after         = app_attrs[service.O_DIE_AFTER],
-        quit_wait         = app_attrs[service.O_QUIT_WAIT],
-        interval          = app_attrs[service.O_RECHECK],
-        poll_interval     = app_attrs[service.O_POLL_INTERVAL],
-        delay             = app_attrs[service.O_ACQUIRE_DELAY],
-        recycled_priority = app_attrs[service.O_RECYCLED_PRIORITY],
-        garbage_lifetime  = app_attrs[service.O_GARBAGE_LIFETIME],
+        workers           = app_opts[service.O_WORKERS],
+        die_after         = app_opts[service.O_DIE_AFTER],
+        quit_wait         = app_opts[service.O_QUIT_WAIT],
+        interval          = app_opts[service.O_RECHECK],
+        poll_interval     = app_opts[service.O_POLL_INTERVAL],
+        delay             = app_opts[service.O_ACQUIRE_DELAY],
+        recycled_priority = app_opts[service.O_RECYCLED_PRIORITY],
+        garbage_lifetime  = app_opts[service.O_GARBAGE_LIFETIME],
     )
     app.run()
 
