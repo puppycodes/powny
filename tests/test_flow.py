437b4404ba4a8a7581c4d552f0213dd6ee6b2a8f
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-03-24 17:31:50 +0400

    test_flow fix: replace delays with condition

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -94,8 +94,7 @@ def _send_recv_event(event):
         # To save events had not numbered sequentially. Check that everything works to increase counter.
         opener.open(_make_event_request({ "_stub": None }))
         opener.open(_make_event_request(event))
-        time.sleep(2)
-        return json.loads(server.get_result().decode())
+        return json.loads(server.wait_for_result().decode())
     finally:
         server.stop()
 
@@ -105,7 +104,20 @@ class _ShotServer(http.server.HTTPServer, threading.Thread):
     def __init__(self, host_name, port):
         http.server.HTTPServer.__init__(self, (host_name, port), _ShotHandler)
         threading.Thread.__init__(self)
-        self.result = None
+        self._result = None
+        self._finished = threading.Condition()
+
+    def wait_for_result(self):
+        with self._finished:
+            while self._result is None:
+                self._finished.wait()
+            return self._result
+
+    def put_result(self, result):
+        assert result is not None, "result should not be None"
+        with self._finished:
+            self._result = result
+            self._finished.notify()
 
     def stop(self):
         _logger.debug("stopping one-shot server %s", self)
@@ -117,23 +129,14 @@ class _ShotServer(http.server.HTTPServer, threading.Thread):
         _logger.debug("starting one-shot server %s", self)
         self.serve_forever()
 
-    def get_result(self):
-        assert self.result is not None, "No result received"
-        return self.result
-
 class _ShotHandler(http.server.BaseHTTPRequestHandler):
     def do_POST(self):
-        if self.server.result is None:
-            self.server.result = self.rfile.read(int(self.headers["Content-Length"]))
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write(b"ok")
-        else:
-            self.send_response(400)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write(b"only one shot!")
+        result = self.rfile.read(int(self.headers["Content-Length"]))
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"ok")
+        self.server.put_result(result)
 
     def log_message(self, format, *args): # pylint: disable=W0622
         _logger.info("%s - - [%s] %s", self.address_string(), self.log_date_time_string(), format % (args))
