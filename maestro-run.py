4ed61c89f4c0eb8cb5a21e69651b62351061ff43
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-03-27 20:41:46 +0400

    maestro-run.py: apostrophes replaced with quotes

--- maestro-run.py
+++ maestro-run.py
@@ -5,14 +5,14 @@ import logging
 from maestro.guestutils import *
 import gns.service
 
-config = gns.service.load_config(os.environ['CONFIG'])
-config['core']['zoo-nodes'] = get_node_list('zookeeper', ports=['client'])
-config['core']['rules-dir'] = os.environ['RULES_DIR']
-config['git']['repo-dir'] = os.environ['REPO_DIR']
+config = gns.service.load_config(os.environ["CONFIG"])
+config["core"]["zoo-nodes"] = get_node_list("zookeeper", ports=["client"])
+config["core"]["rules-dir"] = os.environ["RULES_DIR"]
+config["git"]["repo-dir"] = os.environ["REPO_DIR"]
 
-print('using config: %s' % config)
+print("using config: %s" % config)
 
 gns.service.init_logging(config)
 
-module = importlib.import_module('gns.'+os.environ['MODULE'])
+module = importlib.import_module("gns."+os.environ["MODULE"])
 module.run(config)
