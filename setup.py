f29cb887d48e53244342aad6510d0dab31c7704b
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-04-01 02:25:27 +0400

    setup.py: raava version must be 0.3

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
         ),
 
         install_requires=(
-            "raava >= 0.3",
+            "raava == 0.3",
             "elog >= 0.1",
             "chrpc >= 0.1",
             "meters >= 0.3",
