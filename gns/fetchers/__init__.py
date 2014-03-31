0c5681800da3622fdac8fc298229c2e14f188b6a
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-03-31 16:19:27 +0400

    git fetcher: clone from repo-url if empty, do not require HEAD symlink

--- gns/fetchers/__init__.py
+++ gns/fetchers/__init__.py
@@ -1,4 +1,5 @@
 import os
+import os.path
 import logging
 
 from .. import plugins
@@ -14,11 +15,11 @@ def load_fetchers(config):
 
 def replace_head(rules_path, head_name, module_name):
     head_path = os.path.join(rules_path, head_name)
-    if os.readlink(head_path) == module_name:
+    if os.path.islink(head_path) and os.readlink(head_path) == module_name:
         _logger.debug("HEAD does not need to be updated")
         return
     tmp_path = os.path.join(rules_path, module_name + ".tmp")
-    _logger.info("Updating the rules HEAD: %s -> %s", os.readlink(head_path), module_name)
+    _logger.info("Updating the rules HEAD to %s", module_name)
     os.symlink(module_name, tmp_path)
     try:
         os.rename(tmp_path, head_path)
