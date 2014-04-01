223ccc8d74aecce8f5a25f0a273f6afa4e9aa4f9
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-04-01 22:58:44 +0400

    maestro: ship addresses and image names moved to env files

--- maestro/env-local.sh
+++ maestro/env-local.sh
@@ -1,8 +1,8 @@
-export SHIP_1=zk1.i.fog.yandex.net
-export SHIP_2=zk2.i.fog.yandex.net
-export SHIP_3=zk3.i.fog.yandex.net
+export SHIP_1=localhost
+export SHIP_2=$SHIP_1
+export SHIP_3=$SHIP_1
 
 export ELASTICSEARCH_URL=http://elasticlog.yandex.net:9200
 export YA_SMS_URL=https://golem.yandex-team.ru/api/sms/send
-export DOCKER_ZOOKEEPER_IMAGE=nikicat/zookeeper
+export DOCKER_ZOOKEEPER_IMAGE=nikicat/zookeeper-ut
 export DOCKER_GNS_IMAGE=nikicat/gns-ut
