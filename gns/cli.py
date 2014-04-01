9c83a6cc0d4739e8b16ad0c00a6fca6a65ea2fa3
Devaev Maxim <mdevaev@gmail.com>
2014-04-01 20:15:49 +0400

    Removed copypaste from gns.cli

--- gns/cli.py
+++ gns/cli.py
@@ -1,20 +1,17 @@
-import argparse
 import importlib
+
 from . import service
 
+
+##### Public methods #####
 def main():
-    parser = argparse.ArgumentParser(add_help=False)
+    (config, parser, argv) = service.init()
     parser.add_argument(dest="command")
-    parser.add_argument("-c", "--config", dest="config_file_path", default=None, metavar="<file>")
-
-    options = parser.parse_args()
-
-    config = service.load_config(options.config_file_path)
-    service.init_logging(config)
-    service.init_meters(config)
-
-    importlib.import_module("."+options.command, __package__).run(config)
+    options = parser.parse_args(argv)
+    importlib.import_module("." + options.command, __package__).run(config)
 
 
+##### Main #####
 if __name__ == "__main__":
     main()
+
