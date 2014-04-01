26b1846b6c200aaf1adef3c76cfcf06044d3c346
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-04-01 18:58:18 +0400

    maestro: SMS_URL renamed to YA_SMS_URL, ship addresses moved to env

---
+++ maestro/env-docker.sh
@@ -0,0 +1,6 @@
+export SHIP_1=docker1.i.fog.yandex.net
+export SHIP_2=docker2.i.fog.yandex.net
+export SHIP_3=docker3.i.fog.yandex.net
+
+export ELASTICSEARCH_URL=http://elasticlog.yandex.net:9200
+export YA_SMS_URL=http://localhost # stub
---
+++ maestro/env-zk.sh
@@ -0,0 +1,6 @@
+export SHIP_1=zk1.i.fog.yandex.net
+export SHIP_2=zk2.i.fog.yandex.net
+export SHIP_3=zk3.i.fog.yandex.net
+
+export ELASTICSEARCH_URL=http://elasticlog.yandex.net:9200
+export YA_SMS_URL=https://golem.yandex-team.ru/api/sms/send
