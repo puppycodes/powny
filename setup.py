3a0ff116196d141ceda3b4a35c145babacb7989e
Devaev Maxim <mdevaev@gmail.com>
2015-04-28 17:48:15 +0300

    Bump version: 2.4.0 → 2.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name="powny",
-        version="2.4.0",
+        version="2.5.0",
         url="https://github.com/yandex-sysmon/powny",
         license="GPLv3",
         author="Devaev Maxim",
